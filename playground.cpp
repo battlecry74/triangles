d5087e0782f9453978f9e460c89e3f33a8202d68
miftahulmahfuzh <miftahulmahfuzh@gmail.com>
2016-03-28 15:14:06 +0700

    source code added

---
+++ playground.cpp
@@ -0,0 +1,166 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <GL/glew.h>
+
+#include <glfw3.h>
+GLFWwindow* window;
+
+#include <glm/glm.hpp>
+
+using namespace glm;
+
+#include <common/shader.hpp>
+
+int main( void )
+{
+	// Initialise GLFW
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_RESIZABLE,GL_FALSE); 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // we want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // we dont want the old OpenGL
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow( 1024, 768, "Playground", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window); // Initialize GLEW
+    
+	glewExperimental = true; // Needed for core profile
+	// Initialize GLEW
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+    // Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Dark blue background
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    // Enable depth test
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    // Create a vertex array object
+    GLuint VertexArrayID;//,VertexArrayID2;
+    glGenVertexArrays(1,&VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders("SimpleVertexShader.vertexshader","SimpleFragmentShader.fragmentshader");
+    
+    // An array of 3 vectors which represents 3 vertices
+    static const GLfloat g_vertex_buffer_data[] = {
+        -0.9f, -0.3f, 0.0f,
+        -0.6f, 0.3f, 0.0f,
+        -0.3f, -0.3f, 0.0f,
+        -0.3f, -0.3f, 0.0f,
+        0.0f, 0.3f, 0.0f,
+        0.3f, -0.3f, 0.0f,
+        0.3f, -0.3f, 0.0f,
+        0.6f, 0.3f, 0.0f,
+        0.9f, -0.3f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f
+    };
+
+    static const GLfloat g_color_buffer_data[] = {
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 0.0f, 1.0f
+    };
+
+    // This will identify our vertex buffer 
+    GLuint vertexbuffer,colorbuffer;
+    glGenBuffers(1,&vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER,vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    glGenBuffers(1,&colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER,colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+    bool multi = false;
+	do {
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Use our shader
+        glUseProgram(programID);
+        // 1st attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+              0, // attribute 0. no particular reason for 0. but must match the layout in the shader
+              3, // size
+              GL_FLOAT, // type
+              GL_FALSE, // normalized 
+              0, // stride
+              (void*)0 // array buffer offset
+        );
+        // 2st attribute buffer : colors
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+        glVertexAttribPointer(
+              1, // attribute 1. no particular reason for 1. but must match the layout in the shader
+              3, // size
+              GL_FLOAT, // type
+              GL_FALSE, // normalized 
+              0, // stride
+              (void*)0 // array buffer offset
+        );
+        // Draw the triangle
+        if( (glfwGetKey(window, GLFW_KEY_ENTER) == GLFW_PRESS )) multi = multi==1?0:1;
+        if(multi) {
+            glDrawArrays(GL_TRIANGLES,9,3); 
+        } else {        
+            glDrawArrays(GL_TRIANGLES,0,3); // Starting from vertex 0, 3 vertex total -> 1 triangle
+            glDrawArrays(GL_TRIANGLES,3,3); 
+            glDrawArrays(GL_TRIANGLES,6,3); 
+        }    
+        glDisableVertexAttribArray(0);  
+        glDisableVertexAttribArray(1);  
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+    // Clean up VBO 
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteBuffers(1, &colorbuffer);
+    glDeleteVertexArrays(1, &VertexArrayID);
+    glDeleteProgram(programID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
